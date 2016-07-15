2a5e497ebf3772dc747b3eb6fc35b96498c47d86
Jana Winter <janawhitta@gmail.com>
2016-07-15 19:17:32 +1200

    add media files

---
+++ play.js
@@ -0,0 +1,49 @@
+
+var kick = new audio('kick(2).mp3');
+audio.play();
+
+var snare = new audio('snare(1).mp3');
+audio.play();
+
+var hihat = new audio('hithat(2).mp3');
+audio.play();
+
+var claps = new audio('clap(1).mp3');
+audio.play();
+
+var ride = new audio('cymbal(1).mp3');
+audio.play();
+
+var crash = new audio('cymbal(3).mp3');
+audio.play();
+
+var  tomtom1 = new audio('tom(1).mp3');
+audio.play();
+
+var tomtom2= new audio('tom(3).mp3');
+audio.play();
+
+var tomtom3 = new audio('tom(5).mp3');
+audio.play();
+
+var tomtom4 = new audio('tom(7).mp3');
+audio.play();
+
+var cowbell = new audio('snare(6).mp3');
+audio.play();
+
+var shaker = new audio('shaker(6).mp3');
+audio.play();
+
+
+
+
+
+}
+
+
+
+
+
+
+
