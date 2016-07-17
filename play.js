80347d0ca588a6f5c02ecd5c885f4bfbccdf50db
Jana Winter <janawhitta@gmail.com>
2016-07-17 18:24:05 +1200

    add keypresses

--- play.js
+++ play.js
@@ -1,8 +1,54 @@
 $(document).ready(function(){
 
-$( ".button" ).click(function() {
-    var drum = $(this).attr("id");
-    var audio = new Audio("myaudio/"+drum+".mp3")
-    audio.play();
+    $( ".button" ).click(function() {
+        var drum = $(this).attr("id");
+        var audio = new Audio("myaudio/"+drum+".mp3")
+        audio.play();
+    });
+
+    $(document).keydown(function(e){
+        var drum ="";
+        var char = String.fromCharCode(e.which).toLowerCase();
+        //console.log (e);
+        switch(char){
+            case "a" :
+            drum="kick";
+            break;
+
+            case "s" :
+            drum="snare";
+            break;
+
+            case "d":
+            drum="tomtom1";
+            break;
+
+            case "f":
+            drum="clap";
+            break;
+
+            case "j":
+            drum="hihat";
+            break;
+
+            case "k":
+            drum="cymbal";
+            break;
+
+            case "l":
+            drum="tomtom2"
+            break;
+
+            case ";":
+            drum="tomtom3";
+            break;
+        }
+        if (typeof(drum) !=='undefined'){
+            console.log(drum)
+        $("#"+drum).click();
+        }
+
+    });
 });
-});
+
+
